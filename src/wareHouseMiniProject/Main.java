8ecc18dab1d108890d546cb5125a574f05c261c7
Shukrullah Shirzad <shukrullah.shirzad.779@gmail.com>
2024-11-10 19:12:47 +0430

    create Main class to run the warehouse

---
+++ src/wareHouseMiniProject/Main.java
@@ -0,0 +1,10 @@
+package wareHouseMiniProject;
+
+public class Main {
+    public static void main(String[] args) {
+        Warehouse warehouse = new Warehouse(6);  // Initialize a warehouse with 6 shelves
+
+        warehouse.displayWarehouse();
+
+    }
+}
