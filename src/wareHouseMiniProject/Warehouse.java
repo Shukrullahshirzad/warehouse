a2aa73b2160968abb4952c18c7a93c1b8a3d93b1
Shukrullah Shirzad <shukrullah.shirzad.779@gmail.com>
2024-11-10 19:12:15 +0430

    create warehouse and related methods

--- src/wareHouseMiniProject/Warehouse.java
+++ src/wareHouseMiniProject/Warehouse.java
@@ -1,19 +1,51 @@
 package wareHouseMiniProject;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Warehouse {
-   public Product[] shelf = new Product[6];
+class Warehouse {
+    private List<Shelf> shelves;
 
-   public void addProduct(Product item, int shelfNumber){
-       if(shelf[shelfNumber-1] == null){
-           System.out.println("This shelf already contains " + shelf[shelfNumber-1].getProductName() + "please add put your product in another shelf");
-       }else{
-           shelf[shelfNumber-1] = item;
+    public Warehouse(int numberOfShelves) {
+        shelves = new ArrayList<>();
+        for (int i = 0; i < numberOfShelves; i++) {
+            shelves.add(new Shelf());
+        }
+    }
 
-       }
-   }
-   public String showProducts(){
-       return Arrays.toString(shelf);
-   }
+    // Add product to a specific shelf
+    public void addProductToShelf(int shelfIndex, Product product) {
+        if (shelfIndex >= 0 && shelfIndex < shelves.size()) {
+            shelves.get(shelfIndex).addProduct(product);
+        } else {
+            System.out.println("Shelf index out of range.");
+        }
+    }
+
+    // Import product quantity to a specific shelf
+    public void importProduct(int shelfIndex, String productID, int amount) {
+        if (shelfIndex >= 0 && shelfIndex < shelves.size()) {
+            shelves.get(shelfIndex).importProduct(productID, amount);
+        } else {
+            System.out.println("Shelf index out of range.");
+        }
+    }
+
+    // Export product quantity from a specific shelf
+    public void exportProduct(int shelfIndex, String productID, int amount) {
+        if (shelfIndex >= 0 && shelfIndex < shelves.size()) {
+            shelves.get(shelfIndex).exportProduct(productID, amount);
+        } else {
+            System.out.println("Shelf index out of range.");
+        }
+    }
+
+    // Display all products on all shelves
+    public void displayWarehouse() {
+        for (int i = 0; i < shelves.size(); i++) {
+            System.out.println("Shelf " + (i + 1) + ":");
+            shelves.get(i).displayProducts();
+            System.out.println();
+        }
+    }
 }
